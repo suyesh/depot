112ea2a21853b338b09272fa9e9b97f94098f868
suyeshb@gmail.com <suyeshb@gmail.com>
2014-01-12 16:18:36 -0500

    thy said eff with scss me say no no no lets bootstrap

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass', '~> 3.0.3.0'
+
+group :development do
+	gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '182102f485c6f62fbf4e7172cc923f90cc3c3f1ad3fad3a19b199ab25f12a234dffd533c89f332a5bbd6bf778f311fb063b1e2af44b7ea7d44525351bfb6dfe2'
